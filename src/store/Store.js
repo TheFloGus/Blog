78892d60737106b674405b0ebaf66b325d05ffb5
TheFloGus <jeka467@gmail.com>
2021-07-26 21:21:52 +0300

    fix: store formatting

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,13 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import postsSlice from '../components/slices/PostsSlice';
-import userDataSlice from '../components/slices/userDataSlice';
-import loginSlice from '../components/slices/LoginSlice';
-
+import { configureStore } from "@reduxjs/toolkit";
+import postsSlice from "../components/slices/PostsSlice";
+import userDataSlice from "../components/slices/userDataSlice";
+import loginSlice from "../components/slices/LoginSlice";
 
 export const store = configureStore({
   reducer: {
     posts: postsSlice,
-	userData: userDataSlice,
-	login: loginSlice
+    userData: userDataSlice,
+    login: loginSlice,
   },
 });
